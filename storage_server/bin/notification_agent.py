79959710dbaa5e6b6054e793fc067beb8b8f616b
Ryan M <ryan@ryanmakesrobots.com>
2020-10-27 21:18:42 +0000

    mistype in storage server application

--- storage_server/bin/notification_agent.py
+++ storage_server/bin/notification_agent.py
@@ -7,7 +7,7 @@ HEADERSIZE = 10
 def send_notification(id, table):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((analysisserverip, 7821))
-    data = pickles.dumps((id, table))
+    data = pickle.dumps((id, table))
     data = bytes(f'{len(data):<{HEADERSIZE}}', 'utf-8')+data
     s.send(data)
 
