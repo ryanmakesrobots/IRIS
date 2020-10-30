bed06d297ba8138b744fcf550fe9a673bd56781c
Ryan M <ryan@ryanmakesrobots.com>
2020-10-30 13:03:10 +0000

    removed logging lines

--- analysis_server/bin/analysis_core.py
+++ analysis_server/bin/analysis_core.py
@@ -15,26 +15,18 @@ def face_detection(img):
 
     if 'imagestore/unclassified' not in os.getcwd():
         os.chdir('imagestore/unclassified')
-        print(os.getcwd())
 
     img = cv2.imread(img)
-
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     gray = cv2.equalizeHist(gray)
-
     face = face_cascade.detectMultiScale(gray)
-    print(face)
-    print(len(face))
+
     if len(face) == 0:
         print('no face detected')
         os.remove(fname)
     else:
         for (x,y,w,h) in face:
-            print('entering for vars in face')
             centre = (x + w//2, y + h//2)
             colour_image = cv2.ellipse(img, centre, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
             os.chdir('/face')
             cv2.imwrite(fname, colour_image)
-
-    print('completed')
-
